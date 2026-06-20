143251ec3b62c2bb7c4dfef2a6236cbb932a368f
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2026-06-20 00:33:09 +0000

    agregamosMasValidaciones

--- src/binario/binario.js
+++ src/binario/binario.js
@@ -2,20 +2,25 @@ import { mostrarEnPantalla } from './mostrarEnPantalla.js';
 let results = document.getElementById("resultados");
 export function binario() {
     let number = parseFloat(prompt("Que número quieres convertir a binario"));
-    if(!Number.isNaN(number))
-    {
-        let numeroOriginal = number;
-        let binary = "";
-        while (number > 0) {
-            let remainder = number % 2;
-            //registra los resultados en binary como cadena de texto
-            binary = remainder + binary;
-            number = Math.floor(number / 2);
-        }
-        //lo muestra en pantalla
-        mostrarEnPantalla(`El numero ${numeroOriginal} en binario es ${binary}`);
-    } else
-    {
-        mostrarEnPantalla("ingresa un numero válido");
-    }    
+    if (Number.isNaN(number)) {
+        mostrarEnPantalla("no se puede convertir un texto a binario, ingresa un numero");
+        return
+    }
+    if (number % 1 !== 0) {
+        mostrarEnPantalla("conversiones decimales no disponibles")
+        return
+    }
+    if (number < 0) {
+        mostrarEnPantalla("conversiones de negativos no disponibles");
+    }
+    let numeroOriginal = number;
+    let binary = "";
+    while (number > 0) {
+        let remainder = number % 2;
+        //registra los resultados en binary como cadena de texto
+        binary = remainder + binary;
+        number = Math.floor(number / 2);
+    }
+    //lo muestra en pantalla
+    mostrarEnPantalla(`El numero ${numeroOriginal} en binario es ${binary}`);
 }
--- src/binario/decimal.js
+++ src/binario/decimal.js
@@ -1,15 +1,13 @@
 import { mostrarEnPantalla } from './mostrarEnPantalla.js';
 export function binarioinverso() {
     let numeroAconvertir = prompt("Que número binario quieres convertir a decimal");
-    const cadenaBinaria=/[01]+/
-    if(cadenaBinaria.test(numeroAconvertir))
-    {
+    const cadenaBinaria = /^[01]+\$/
+    if (cadenaBinaria.test(numeroAconvertir)) {
         let numeroOriginal = numeroAconvertir;
         numeroAconvertir = parseInt(numeroAconvertir, 2);
         mostrarEnPantalla(`El numero ${numeroOriginal} en base 10 es ${numeroAconvertir}`);
     }
-    else
-    {
+    else {
         mostrarEnPantalla("Ingresa un numero válido");
     }
 }
